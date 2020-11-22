715bc3b4f479fbc9729224ebd12715f9f38b20bd
Rustam Mamedov <mamedov.rust@gmail.com>
2020-11-22 22:20:11 +0200

    Added CoreData

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Rustam Mamedov on 21.11.2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+    
+}
